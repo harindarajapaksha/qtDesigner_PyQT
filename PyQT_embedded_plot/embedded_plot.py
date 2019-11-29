42fbdf069a192d6228a8173fb5d42583f4369739
Neo <k.rajapaksha@latrobe.edu.au>
2019-11-29 22:24:26 +1100

    initial imports

---
+++ PyQT_embedded_plot/embedded_plot.py
@@ -0,0 +1,9 @@
+import sys
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib import pyplot as plt
+
+import matplotlib
+matplotlib.use('QT5Agg')
+
+from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.uic import loadUi
