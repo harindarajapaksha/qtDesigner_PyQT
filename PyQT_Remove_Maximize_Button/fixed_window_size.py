52d15fdb969ec39009b27628d716a286348609e4
Kolin Harinda Rajapaksha <58077419+harindarajapaksha@users.noreply.github.com>
2023-08-17 18:29:19 +1000

    Add files via upload

---
+++ PyQT_Remove_Maximize_Button/fixed_window_size.py
@@ -0,0 +1,49 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+
+        """
+        ______ This will remove the 'Maximize Button' ______
+        
+        * Comment out the MainWindow.resize(width, height)
+        * Add the following line 
+            MainWindow.setFixedSize(width, height)
+        
+        """
+        # MainWindow.resize(853, 468)
+        MainWindow.setFixedSize(853, 468)
+
+
+
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 853, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Fixed Window Size"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
